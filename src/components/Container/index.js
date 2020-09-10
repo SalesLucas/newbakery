94a13207390bd8fe0085bf6e00546c1aff51cbce
lucas <lucasmarthaussales@hotmail.com>
2020-09-10 10:39:07 -0300

    Container Commit

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -5,8 +5,8 @@ import './Container.css';
 
 function Container() {
     return (
-        <div class="Container">
-            <div class="container marketing">
+        <div className="Container">
+            <div class="text-center">
                 <div class="row">
                     <div class="col-lg-4">
                         <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
